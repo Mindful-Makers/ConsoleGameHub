ad0ffefd3f02c06909b2c066cc7e800e0ff92ff6
JaidenSearle <jaidensearle@gmail.com>
2025-10-21 20:46:59 -0400

    GameHistoryTracker junit tests

---
+++ GameHistoryTrackerTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+/**
+ * Tests for GameHistoryTracker
+ *
+ */
+
+public class GameHistoryTrackerTest {
+
+    @Test
+    void TestRecordPlay() {
+
+
+    }
+
+    @Test
+    void TestDisplayHistory() {
+
+
+    }
+
+    @Test
+    void TestSaveHistory() {
+
+
+    }
+
+    @Test
+    void TestLoadHistory() {
+
+
+    }
+
+    @Test
+    void TestClearHistory() {
+
+
+    }
+}
