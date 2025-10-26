b415ed2f371d8f56bb79f7b026c27d86199d30d3
oowolabi@msudenver.edu <oowolabi@msudenver.edu>
2025-10-26 03:03:29 -0400

    MasterMIndGame with hidden random generator for issue #44

--- MasterMindGame.java
+++ MasterMindGame.java
@@ -1,24 +1,21 @@
 import java.util.Optional;
 
-/**
- * A code-breaking game where the app selects a sequence of symbols, and
- * the player tries to guess the sequence within a fixed number of attempts.
- *
- * Feedback for each guess indicates how many values are correct and in
- * the correct position and how many are correct but in the wrong position.
- *
- * Consider using arrays or strings to store and compare codes.
- * @version 1
- */
 class MasterMindGame implements Game {
+    private stringGen  generator; 
+    
+    public MasterMindGame() { 
+        this.generator = new stringGen();
+    }
+    
     @Override
     public String getName() {
         return "MasterMind";
     }
-
+    
     @Override
     public Optional<Integer> play() {
         System.out.println("[Playing MasterMind - Placeholder]");
+        generator.generateHiddenCode(4);
         return Optional.empty();
     }
 }
--- stringGen.java
+++ stringGen.java
@@ -1,39 +1,30 @@
 import java.util.Random;
 
 public class stringGen {
-
-    public class randGenerator {
-        private String hiddenCode;  
-        
-        public String generateRandomString(int length) {
-            String character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            Random rands = new Random();
-            StringBuilder build = new StringBuilder(length);
-
-            for (int i = 0; i < length; i++) {
-                int index = rands.nextInt(character.length());
-                build.append(character.charAt(index));
-            }
-            return build.toString();
-        }
-        
-        
-        public void generateCode() {
-            this.hiddenCode = generateRandomString(4);
-        }
-        
+    private Random random;
+    private String hiddenCode;  
     
-        public String getHiddenCode() {
-            return hiddenCode;
+    public stringGen() {
+        this.random = new Random(); 
+        this.hiddenCode = null;
+    }
+    
+    public void generateHiddenCode(int length) {  
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder builder = new StringBuilder(length);  
+        
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());  
+            builder.append(characters.charAt(index));
         }
         
-        public static void main(String[] args) {
-            stringGen outer = new stringGen();
-            randGenerator gen = outer.new randGenerator();
-            
-            gen.generateCode(); 
-            System.out.println("Code generated and hidden!");
-            
+        this.hiddenCode = builder.toString();
+    }
+    
+    public boolean checkCode(String guess) {
+        if (hiddenCode == null) {
+            return false;
         }
+        return hiddenCode.equals(guess);
     }
 }
